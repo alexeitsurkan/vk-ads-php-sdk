7ca52eaa9458d8de85f1b21396ec2f52f5442fe1
alex <alexeitsurkan@gmail.com>
2023-04-12 12:56:55 +0300

    bugfix

--- src/services/statistics/enum/StatisticObjectEnum.php
+++ src/services/statistics/enum/StatisticObjectEnum.php
@@ -6,7 +6,7 @@ use VkAdsPhpSdk\components\Enum;
 
 class StatisticObjectEnum extends Enum
 {
-    public const DAY       = 'banners';
+    public const BANNERS       = 'banners';
     public const AD_GROUPS = 'ad_groups';
     public const AD_PLANS  = 'ad_plans';
     public const USERS     = 'users';
