2ca6d37a9361d0e82b89e0d8d135b7f2456396e3
alex <alexeitsurkan@gmail.com>
2023-04-20 17:50:08 +0300

    bugfix billing transaction

--- src/models/TransactionAddItem.php
+++ src/models/TransactionAddItem.php
@@ -3,6 +3,7 @@
 namespace VkAdsPhpSdk\models;
 
 use VkAdsPhpSdk\components\Model;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class TransactionAddItem extends Model
 {
--- src/services/billing/services/transactions/TransactionsService.php
+++ src/services/billing/services/transactions/TransactionsService.php
@@ -12,6 +12,6 @@ class TransactionsService extends BaseService
     public function post(int $user_id, TransactionModeEnum $mode, TransactionAddItem $model)
     {
         $response = $this->doPost("/api/v2/billing/transactions/$mode/$user_id.json", $model);
-        return $this->mapArray($response, TransactionGetItem::class);
+        return $this->map($response, TransactionGetItem::class);
     }
 }
